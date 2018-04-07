3b9aaffc9a2e7e7f100dc9bdedaf50a893c1c487
Torak28 <zelechowski28@gmail.com>
2018-04-07 17:15:40 +0200

    Add debug mode to draw

--- Cube/draw.py
+++ Cube/draw.py
@@ -11,6 +11,13 @@ class CubeDraw:
     grayC = (127, 127, 127)
     blackC = (0, 0, 0)
 
+    W = 'W'
+    G = 'G'
+    O = 'O'
+    R = 'R'
+    B = 'B'
+    Y = 'Y'
+
     img = None
 
     def __init__(self, size_of_tile=None):
@@ -30,7 +37,33 @@ class CubeDraw:
         draw.rectangle(xy, fill=color, outline=self.blackC)
         return img
 
-    def draw_sign(self, img):
+    def draw_tile_debug(self, img, coord, color, number, face):
+        """Wyrysowanie pojedynczej plytki - coord to gorny lewy rog"""
+        print(coord, number, face)
+        draw = ImageDraw.Draw(img)
+        xy = (coord, (coord[0] + self.size_of_tile, coord[1] + self.size_of_tile))
+        font = ImageFont.truetype("arial.ttf", size=20)
+        draw.rectangle(xy, fill=color, outline=self.blackC)
+        if face is 0:
+            face = 'F'
+        elif face is 1:
+            face = 'D'
+        elif face is 2:
+            face = 'L'
+        elif face is 3:
+            face = 'R'
+        elif face is 4:
+            face = 'U'
+        elif face is 5:
+            face = 'B'
+        if number is 4:
+            number = face
+        draw.text((coord[0] + self.size_of_tile//3, coord[1] + self.size_of_tile//3),
+                  str(number), font=font, fill=self.blackC)
+        return img
+
+    @staticmethod
+    def draw_sign(img):
         """Podpisanie  rysunku"""
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("arial.ttf", size=15)
@@ -68,17 +101,63 @@ class CubeDraw:
         self.img = self.draw_init()
         for i in range(len(state)):
             for j in range(len(state[i])):
-                if state[i][j] == 'W':
+                if state[i][j] is self.W:
                     self.draw_tile(self.img, all_coord[i][j], self.whiteC)
-                elif state[i][j] == 'G':
+                elif state[i][j] is self.G:
                     self.draw_tile(self.img, all_coord[i][j], self.greenC)
-                elif state[i][j] == 'O':
+                elif state[i][j] is self.O:
                     self.draw_tile(self.img, all_coord[i][j], self.orangeC)
-                elif state[i][j] == 'R':
+                elif state[i][j] is self.R:
                     self.draw_tile(self.img, all_coord[i][j], self.redC)
-                elif state[i][j] == 'B':
+                elif state[i][j] is self.B:
                     self.draw_tile(self.img, all_coord[i][j], self.blueC)
-                elif state[i][j] == 'Y':
+                elif state[i][j] is self.Y:
                     self.draw_tile(self.img, all_coord[i][j], self.yellowC)
+
+        return self.draw_sign(self.img)
+
+    def draw_state_debug(self, state):
+        """Rysowanie calej kostki"""
+        white_coord = [(3 * self.size_of_tile, 3 * self.size_of_tile), (4 * self.size_of_tile, 3 * self.size_of_tile), (5 * self.size_of_tile, 3 * self.size_of_tile),
+                       (3 * self.size_of_tile, 4 * self.size_of_tile), (4 * self.size_of_tile, 4 * self.size_of_tile), (5 * self.size_of_tile, 4 * self.size_of_tile),
+                       (3 * self.size_of_tile, 5 * self.size_of_tile), (4 * self.size_of_tile, 5 * self.size_of_tile), (5 * self.size_of_tile, 5 * self.size_of_tile)]
+
+        green_coord = [(0 * self.size_of_tile, 3 * self.size_of_tile), (1 * self.size_of_tile, 3 * self.size_of_tile), (2 * self.size_of_tile, 3 * self.size_of_tile),
+                       (0 * self.size_of_tile, 4 * self.size_of_tile), (1 * self.size_of_tile, 4 * self.size_of_tile), (2 * self.size_of_tile, 4 * self.size_of_tile),
+                       (0 * self.size_of_tile, 5 * self.size_of_tile), (1 * self.size_of_tile, 5 * self.size_of_tile), (2 * self.size_of_tile, 5 * self.size_of_tile)]
+
+        orange_coord = [(3 * self.size_of_tile, 0 * self.size_of_tile), (4 * self.size_of_tile, 0 * self.size_of_tile), (5 * self.size_of_tile, 0 * self.size_of_tile),
+                        (3 * self.size_of_tile, 1 * self.size_of_tile), (4 * self.size_of_tile, 1 * self.size_of_tile), (5 * self.size_of_tile, 1 * self.size_of_tile),
+                        (3 * self.size_of_tile, 2 * self.size_of_tile), (4 * self.size_of_tile, 2 * self.size_of_tile), (5 * self.size_of_tile, 2 * self.size_of_tile)]
+
+        red_coord = [(3 * self.size_of_tile, 6 * self.size_of_tile), (4 * self.size_of_tile, 6 * self.size_of_tile), (5 * self.size_of_tile, 6 * self.size_of_tile),
+                     (3 * self.size_of_tile, 7 * self.size_of_tile), (4 * self.size_of_tile, 7 * self.size_of_tile), (5 * self.size_of_tile, 7 * self.size_of_tile),
+                     (3 * self.size_of_tile, 8 * self.size_of_tile), (4 * self.size_of_tile, 8 * self.size_of_tile), (5 * self.size_of_tile, 8 * self.size_of_tile)]
+
+        blue_coord = [(6 * self.size_of_tile, 3 * self.size_of_tile), (7 * self.size_of_tile, 3 * self.size_of_tile), (8 * self.size_of_tile, 3 * self.size_of_tile),
+                      (6 * self.size_of_tile, 4 * self.size_of_tile), (7 * self.size_of_tile, 4 * self.size_of_tile), (8 * self.size_of_tile, 4 * self.size_of_tile),
+                      (6 * self.size_of_tile, 5 * self.size_of_tile), (7 * self.size_of_tile, 5 * self.size_of_tile), (8 * self.size_of_tile, 5 * self.size_of_tile)]
+
+        yellow_coord = [(9 * self.size_of_tile, 3 * self.size_of_tile), (10 * self.size_of_tile, 3 * self.size_of_tile), (11 * self.size_of_tile, 3 * self.size_of_tile),
+                        (9 * self.size_of_tile, 4 * self.size_of_tile), (10 * self.size_of_tile, 4 * self.size_of_tile), (11 * self.size_of_tile, 4 * self.size_of_tile),
+                        (9 * self.size_of_tile, 5 * self.size_of_tile), (10 * self.size_of_tile, 5 * self.size_of_tile), (11 * self.size_of_tile, 5 * self.size_of_tile)]
+
+        all_coord = [white_coord, green_coord, orange_coord, red_coord, blue_coord, yellow_coord]
+
+        self.img = self.draw_init()
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if state[i][j] is self.W:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.whiteC, j, i)
+                elif state[i][j] is self.G:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.greenC, j, i)
+                elif state[i][j] is self.O:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.orangeC, j, i)
+                elif state[i][j] is self.R:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.redC, j, i)
+                elif state[i][j] is self.B:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.blueC, j, i)
+                elif state[i][j] is self.Y:
+                    self.draw_tile_debug(self.img, all_coord[i][j], self.yellowC, j, i)
 
         return self.draw_sign(self.img)
